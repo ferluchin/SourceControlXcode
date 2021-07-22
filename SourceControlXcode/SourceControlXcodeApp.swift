e142e8062854879792803558c0c4127a214f5763
ferluchin <lgrandab@gmail.com>
2021-07-22 13:06:31 -0500

    Initial Commit

---
+++ SourceControlXcode/SourceControlXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcodeApp.swift
+//  SourceControlXcode
+//
+//  Created by Luis Granda on 22/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
